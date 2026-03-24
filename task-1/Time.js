5e71d6eb0e2884a5adc68df9a176741822f7af60
Halyna Romanyshyn <30292604+halyna1995@users.noreply.github.com>
2026-03-24 22:21:26 +0100

    task 1 complete

--- task-1/Time.js
+++ task-1/Time.js
@@ -1,3 +1,57 @@
 export class Time {
-  // Your code here
+    #secondsFromMidnight;
+
+  constructor(hours, minutes, seconds) {
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+    if (totalSeconds < 0 || totalSeconds >= 86400) {
+      throw new Error('Invalid time');
+    }
+
+    this.#secondsFromMidnight = totalSeconds;
+  }
+
+  getHours() {
+    return Math.floor(this.#secondsFromMidnight / 3600);
+  }
+
+  getMinutes() {
+    return Math.floor((this.#secondsFromMidnight % 3600) / 60);
+  }
+
+  getSeconds() {
+    return this.#secondsFromMidnight % 60;
+  }
+
+  addSeconds(seconds) {
+    this.#secondsFromMidnight =
+      ((this.#secondsFromMidnight + seconds) % 86400 + 86400) % 86400;
+  }
+
+  addMinutes(minutes) {
+    this.addSeconds(minutes * 60);
+  }
+
+  addHours(hours) {
+    this.addSeconds(hours * 3600);
+  }
+
+  toString() {
+  let hour = this.getHours();
+  let minute = this.getMinutes();
+  let second = this.getSeconds();
+
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+
+  if (minute < 10) {
+    minute = '0' + minute;
+  }
+
+  if (second < 10) {
+    second = '0' + second;
+  }
+    return `${hour}:${minute}:${second}`;
+  }
 }
